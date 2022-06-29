280a09bbf8d3183a004b89d9f30ec1a13edfbccd
sunsfrom <dingxingu@gmail.com>
2022-06-29 20:57:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,9 +11,9 @@
 #
 
 # Uncomment a feed source
-sed -i "/helloworld/d" "feeds.conf.default"
+#sed -i "/helloworld/d" "feeds.conf.default"
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
-
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> "feeds.conf.default"
 
 #sed -i '$a src-git lienol https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 
